efaaee7afea9453f890fef4fc540cc6f4f52c739
bhavanabc05 <bhavanabc05@gmail.com>
2025-02-11 19:43:37 +0530

    Day 2 React

---
+++ Day2_React/Examples/src/App.jsx
@@ -0,0 +1,14 @@
+import Greetings from "./Greetings";
+import Navbar from "./header/Navbar";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <h1><marquee> Welcome to react</marquee></h1>
+      <Greetings />
+
+    </>
+  );
+}
+export default App;
